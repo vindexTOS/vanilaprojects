a45133b2be60a676ae1b788fde8c7ac7db18fa37
VindexTOS <giorgikutateladze1998@gmail.com>
2023-05-01 23:35:56 +0400

    api added

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const app = require('express')()
 const fs = require('fs')
 
+const data = [
+  {
+    name: 'gio',
+    id: '1',
+  },
+  {
+    name: 'go',
+    id: '2',
+  },
+]
+
 app.get('/', (req, res) => {
   fs.readFile('./views/index.html', (err, data) => {
     res.setHeader('Content-Type', 'text/html')
@@ -12,6 +23,15 @@ app.get('/tabata', (req, res) => {
   fs.readFile('./views/Tabata.html', (err, data) => {
     res.setHeader('Content-Type', 'text/html')
     res.status(200).send(data)
+  })
+})
+
+app.get('/api/v1/:id', (req, res) => {
+  const { id } = req.params
+  const person = data.find((val) => val.id === id)
+  res.json({
+    res: true,
+    data: person,
   })
 })
 
